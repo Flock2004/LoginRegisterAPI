cdb8269ad6c6af6bbb33efecd960a9392efbd6c3
Velonjara Francelot <francelot.velonjara@etu.u-pec.fr>
2022-03-25 15:30:51 +0000

    Upload New File

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/users')
+const dotenv = require('dotenv')
+
+var app = express()
+
+dotenv.config()
+
+mongoose.connect('mongodb+srv://strategin:strategin@cluster0.h5i1i.mongodb.net/user?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+app.listen(8000)
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+  next()
+})
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', userRoutes)
+
+app.use(function(req, res, next) {
+  next(createError(404))
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+});
+
+module.exports = app
