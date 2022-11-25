5307364dbe6cfb7ec64aeaaf8d269489bc5346a4
Flock2004 <francelot.velonjara@etu.u-pec.fr>
2022-11-26 00:09:44 +0100

    remove comment

--- app.js
+++ app.js
@@ -38,12 +38,10 @@ app.use('/', userRoutes)
 app.use(function(req, res, next) {
   next(createError(404))
 });
-
 app.use(function(err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
   res.status(err.status || 500)
   res.render('error')
 });
